50c1b8fca1102f26fe0f18ff1b124b2c9c50cad8
Alejandro Esteban Celdrán <aestebance@uoc.edu>
2021-05-28 02:03:20 +0200

    adding styles and about and contact module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '', loadChildren: () =>
-      import('./@pages/home/home.module').then(m => m.HomeModule)
+      import('./@pages/home/home.module').then(m => m.HomeModule), pathMatch: 'full'
+  },
+  {
+    path: 'about', loadChildren: () =>
+      import('./@pages/about/about.module').then(m => m.AboutModule)
+  },
+  {
+    path: 'contact', loadChildren: () =>
+      import('./@pages/contact/contact.module').then(m => m.ContactModule)
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CoreModule} from "./core/core.module";
+import { AboutComponent } from './@pages/about/about.component';
+import { ContactComponent } from './@pages/contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
