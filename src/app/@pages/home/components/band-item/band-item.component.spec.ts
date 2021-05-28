4e9a3e6b017538bc7a48216a902e36b4916d3ae0
Alejandro Esteban Celdrán <aestebance@uoc.edu>
2021-05-28 16:16:31 +0200

    added components in @pages/home

---
+++ src/app/@pages/home/components/band-item/band-item.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BandItemComponent } from './band-item.component';
+
+describe('BandItemComponent', () => {
+  let component: BandItemComponent;
+  let fixture: ComponentFixture<BandItemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BandItemComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BandItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/@pages/home/components/band-item/band-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-band-item',
+  templateUrl: './band-item.component.html',
+  styleUrls: ['./band-item.component.scss']
+})
+export class BandItemComponent implements OnInit {
+  @Input() band : any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
