a24b6a33395264f3722bf96c17b5eab091c52d19
Alejandro Esteban Celdrán <aestebance@uoc.edu>
2021-05-30 16:18:44 +0200

    added youtube video player and other stuffs

--- src/app/@pages/home/home.module.ts
+++ src/app/@pages/home/home.module.ts
@@ -8,14 +8,18 @@ import {CoreModule} from "../../core/core.module";
 import { BandDetailComponent } from './pages/band-detail/band-detail.component';
 import { BandEditComponent } from './pages/band-edit/band-edit.component';
 import { BandsGalleryComponent } from './components/bands-gallery/bands-gallery.component';
-
+import { YoutubePlayerComponent } from './pages/band-detail/components/youtube-player/youtube-player.component';
+import {YouTubePlayerModule} from '@angular/youtube-player';
 
 @NgModule({
-  declarations: [HomeComponent, BandItemComponent, BandDetailComponent, BandEditComponent, BandsGalleryComponent],
+  declarations: [HomeComponent, BandItemComponent, BandDetailComponent, BandEditComponent, BandsGalleryComponent, YoutubePlayerComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    CoreModule
+    CoreModule,
+    YouTubePlayerModule
   ]
 })
 export class HomeModule { }
+
+let apiLoaded = false;
---
+++ src/app/@pages/home/pages/band-detail/components/youtube-player/youtube-player.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-youtube-player',
+  templateUrl: './youtube-player.component.html',
+  styleUrls: ['./youtube-player.component.scss']
+})
+export class YoutubePlayerComponent implements OnInit {
+  @Input() ytClip: any;
+  apiLoaded = false;
+  constructor() { }
+
+  ngOnInit(): void {
+    if (!this.apiLoaded) {
+      // This code loads the IFrame Player API code asynchronously, according to the instructions at
+      // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
+      const tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
+      document.body.appendChild(tag);
+      this.apiLoaded = true;
+    }
+  }
+}
