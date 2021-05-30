77c4fd9165cfadbdcff953ba1972ba75837847e7
Alejandro Esteban Celdrán <aestebance@uoc.edu>
2021-05-30 13:44:04 +0200

    added bands gallery component to home

--- src/app/@pages/home/home.module.ts
+++ src/app/@pages/home/home.module.ts
@@ -7,10 +7,11 @@ import { BandItemComponent } from './components/band-item/band-item.component';
 import {CoreModule} from "../../core/core.module";
 import { BandDetailComponent } from './pages/band-detail/band-detail.component';
 import { BandEditComponent } from './pages/band-edit/band-edit.component';
+import { BandsGalleryComponent } from './components/bands-gallery/bands-gallery.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, BandItemComponent, BandDetailComponent, BandEditComponent],
+  declarations: [HomeComponent, BandItemComponent, BandDetailComponent, BandEditComponent, BandsGalleryComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
