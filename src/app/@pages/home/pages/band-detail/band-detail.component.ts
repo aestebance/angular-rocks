b70f7ba3d0ad607a04ccb54e498d42dddaeb8554
Alejandro Esteban Celdrán <aestebance@uoc.edu>
2021-05-31 17:15:09 +0200

    modifying band-details component

--- src/app/@pages/home/pages/band-detail/band-detail.component.ts
+++ src/app/@pages/home/pages/band-detail/band-detail.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {BandsService} from "../../../../shared/services/bands.service";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {PrimeNGConfig} from "primeng/api";
+import {ConfirmationService} from 'primeng/api';
+import {Message} from 'primeng/api';
 
 @Component({
   selector: 'app-band-detail',
   templateUrl: './band-detail.component.html',
-  styleUrls: ['./band-detail.component.scss']
+  styleUrls: ['./band-detail.component.scss'],
+  providers: [ConfirmationService]
 })
 export class BandDetailComponent implements OnInit {
   bandList: any;
   bandId: any;
   band: any;
+  msgs: Message[] = [];
 
-  constructor(private primengConfig: PrimeNGConfig, private route: ActivatedRoute, private bandsService: BandsService) {
+  constructor(private confirmationService: ConfirmationService, private primengConfig: PrimeNGConfig, private route: ActivatedRoute, private router: Router, private bandsService: BandsService) {
     this.route.paramMap.subscribe(params => {
       if (params.get('bandId')) {
         this.bandId = params.get('bandId');
@@ -27,10 +31,37 @@ export class BandDetailComponent implements OnInit {
     });
   }
 
+  confirmDelete() {
+    this.confirmationService.confirm({
+      message: 'Are you sure that you want to delete this record?',
+      header: 'Delete Confirmation',
+      icon: 'pi pi-info-circle',
+      accept: () => {
+        this.msgs = [{severity: 'info', summary: 'Confirmed', detail: 'Record deleted'}];
+        this.deleteBand();
+      },
+      reject: () => {
+        this.msgs = [{severity: 'info', summary: 'Rejected', detail: 'You have rejected'}]
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.primengConfig.ripple = true;
     window.scroll(0, 0);
   }
 
+  deleteBand(): void {
+    this.bandsService.deleteBand(this.bandId);
+    this.router.navigate(['/']);
+  }
+
+  editBand(): void {
+    this.router.navigate(['/edit/' + this.bandId]);
+  }
+
+  newBand() : void {
+    let newId = this.bandList.length;
+    this.router.navigate(['/edit/' + newId]);
+  }
 }
