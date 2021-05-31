cd8078ea2eef366b81cf5f6c90f24b8133ed1f1f
Alejandro Esteban Celdrán <aestebance@uoc.edu>
2021-06-01 01:21:08 +0200

    changed size of youtube player

--- src/app/@pages/home/home.module.ts
+++ src/app/@pages/home/home.module.ts
@@ -7,7 +7,7 @@ import {CoreModule} from "../../core/core.module";
 import { BandDetailComponent } from './pages/band-detail/band-detail.component';
 import { BandEditComponent } from './pages/band-edit/band-edit.component';
 import { BandsGalleryComponent } from './components/bands-gallery/bands-gallery.component';
-import { YoutubePlayerComponent } from './pages/band-detail/components/youtube-player/youtube-player.component';
+import { YoutubePlayerComponent } from './components/youtube-player/youtube-player.component';
 import {YouTubePlayerModule} from '@angular/youtube-player';
 import {CardModule} from "primeng/card";
 import {ButtonModule} from "primeng/button";
